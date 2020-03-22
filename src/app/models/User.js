63fd70449d00f078a33126c31ed300160b5c5135
claudio-code <suporte2424@outlook.com>
2020-03-22 11:51:51 -0300

    criado model do user

---
+++ src/app/models/User.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model{
+  static init(sequelize) {
+    super.init({
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+      },
+      {
+      sequelize
+    });
+    return this;
+  }
+}
+
+export default User;
