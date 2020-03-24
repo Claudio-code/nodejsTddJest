fa35cc2fd9ade77700bc3d4a85a22b3d29a5a878
claudio-code <suporte2424@outlook.com>
2020-03-24 06:15:06 -0300

    criado teste de email duplicado e arquivo para limpar sqlite apos cada teste

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import app from '../../src/app';
+import truncate from '../util/truncate';
 
 describe('User', async () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +17,21 @@ describe('User', async () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with dublicated email', async () => {
+    await request(app).post('/users').send({
+        name: 'Claudio Silva Junior',
+        email: 'claudio@digitalone.com.br',
+        password_hash: '123',
+      });
+
+    const response = await request(app).post('/users').send({
+        name: 'Claudio Silva Junior',
+        email: 'claudio@digitalone.com.br',
+        password_hash: '123',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,16 @@
 import User from '../models/User';
+import { where } from 'sequelize';
 
 class UserController {
   async store(req, res) {
+    const { email } = req.body;
+    const checkEmail = await User.findOne({ where: { email } });
+
+    if (checkEmail) {
+      return res.status(400).json({ error: 'duplicate email from user' });
+    }
+
+
     const user = await User.create(req.body);
 
     return res.json(user);
